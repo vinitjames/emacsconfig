e588c774a804b1ba18c18c16a2ef1927a0a34b81
vinit <vinit.james24@gmail.com>
2020-04-27 19:49:12 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,35 @@
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(load-file (concat user-emacs-directory "~/.emacs.d/cedet/cedet-devel-load.el"))
+(load-file (concat user-emacs-directory "~/.emacs.d/cedet/contrib/cedet-contrib-load.el"))
+
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 4) ; Assuming you want your tabs to be four spaces wide
+(defvaralias 'c-basic-offset 'tab-width)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-checker-error-threshold 5000)
+ '(package-selected-packages
+   (quote
+    (yasnippet-snippets flycheck which-key use-package auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
