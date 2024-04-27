8e39a9c81e508df47fde629658621a30ee4c4c70
vinit <vinit@vinit.arch.host>
2024-04-27 10:42:24 +0000

    modified init.el

--- init.el
+++ init.el
@@ -11,8 +11,8 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-(load-file (concat user-emacs-directory "~/.emacs.d/cedet/cedet-devel-load.el"))
-(load-file (concat user-emacs-directory "~/.emacs.d/cedet/contrib/cedet-contrib-load.el"))
+;;(load-file (concat user-emacs-directory "~/.emacs.d/cedet/cedet-devel-load.el"))
+;;(load-file (concat user-emacs-directory "~/.emacs.d/cedet/contrib/cedet-contrib-load.el"))
 
 (setq-default indent-tabs-mode t)
 (setq-default tab-width 4) ; Assuming you want your tabs to be four spaces wide
@@ -26,10 +26,13 @@
  '(flycheck-checker-error-threshold 5000)
  '(package-selected-packages
    (quote
-    (yasnippet-snippets flycheck which-key use-package auto-complete))))
+	(yasnippet-snippets flycheck which-key use-package auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Clanng format
+
